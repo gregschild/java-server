21e5acbbc81507235190741103c5ec9fc5ce3a47
Greg Schild <greg.schild@skybettingandgaming.com>
2016-10-19 10:36:39 +0100

    initial commit

---
+++ docker/docker_build.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+set +e
+docker stop java-server
+docker rm java-server
+set -e
+
+pushd ~/java/docker/java-server
+docker build --tag java-server:latest .
+popd
+
+echo "Running MYSQL Server"
+
+docker run --name java-server -p 8080:8080 -p 9990:9990 -d java-server:latest
